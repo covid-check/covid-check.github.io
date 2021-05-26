d4db93aa485a7648a137d8bff99912da390fee79
aman220320072007 <75264859+aman220320072007@users.noreply.github.com>
2021-05-26 08:26:36 +0530

    Update mobile.js

--- mobile.js
+++ mobile.js
@@ -1,24 +1,46 @@
-screen_width=screen.width
-screen_height=screen.height
-console.log(screen_width,screen_height)
-if(screen_width!==1536 && screen_height!==864){
-    document.body.style.backgroundImage="url('COVID-19-CDC.resized.rotated.png')";
-    document.getElementById("text1").style.color='black'
-    document.getElementById("text2").style.color='black'
-    document.getElementById("text3").style.color='black'
-    document.getElementById("header").style.color='#054683'
-    document.getElementById("header").style.fontFamily='Delicious'
-    document.getElementById("text1").style.float='none'
-    document.getElementById("totalconfirmed").style.marginLeft=0
-    document.getElementById("text2").style.float='none'
-    document.getElementById("totaldeaths").style.marginLeft=0
-    document.getElementById("text3").style.float='none'
-    document.getElementById("totalrecovered").style.marginLeft=0
-    document.getElementById('text1').style.textAlign='left'
-    document.getElementById('text2').style.textAlign='left'
-    document.getElementById('text3').style.textAlign='left'
-    document.getElementById('header').style.textAlign='centre'
-    document.getElementById('header').style.marginLeft=110
-    document.body.style.zoom = "50%";
+function getMobileOperatingSystem() {
+ const userAgent = navigator.userAgent || navigator.vendor;
+  if (['ipad', 'iphone', 'ipod'].some(e => userAgent.toLowerCase().includes(e))) {
+       document.body.style.backgroundImage="url('COVID-19-CDC.resized.rotated.png')";
+      document.getElementById("text1").style.color='black'
+      document.getElementById("text2").style.color='black'
+      document.getElementById("text3").style.color='black'
+      document.getElementById("header").style.color='#054683'
+      document.getElementById("header").style.fontFamily='Delicious'
+      document.getElementById("text1").style.float='none'
+      document.getElementById("totalconfirmed").style.marginLeft=0
+      document.getElementById("text2").style.float='none'
+      document.getElementById("totaldeaths").style.marginLeft=0
+      document.getElementById("text3").style.float='none'
+      document.getElementById("totalrecovered").style.marginLeft=0
+      document.getElementById('text1').style.textAlign='left'
+      document.getElementById('text2').style.textAlign='left'
+      document.getElementById('text3').style.textAlign='left'
+      document.getElementById('header').style.textAlign='centre'
+      document.getElementById('header').style.marginLeft=110
+      document.body.style.zoom = "50%";
+} else if (userAgent.toLowerCase().includes('android')) {
 
+      document.body.style.backgroundImage="url('COVID-19-CDC.resized.rotated.png')";
+      document.getElementById("text1").style.color='black'
+      document.getElementById("text2").style.color='black'
+      document.getElementById("text3").style.color='black'
+      document.getElementById("header").style.color='#054683'
+      document.getElementById("header").style.fontFamily='Delicious'
+      document.getElementById("text1").style.float='none'
+      document.getElementById("totalconfirmed").style.marginLeft=0
+      document.getElementById("text2").style.float='none'
+      document.getElementById("totaldeaths").style.marginLeft=0
+      document.getElementById("text3").style.float='none'
+      document.getElementById("totalrecovered").style.marginLeft=0
+      document.getElementById('text1').style.textAlign='left'
+      document.getElementById('text2').style.textAlign='left'
+      document.getElementById('text3').style.textAlign='left'
+      document.getElementById('header').style.textAlign='centre'
+      document.getElementById('header').style.marginLeft=110
+      document.body.style.zoom = "50%";
+} else {
+  console.log('Other');
 }
+}
+getMobileOperatingSystem()
